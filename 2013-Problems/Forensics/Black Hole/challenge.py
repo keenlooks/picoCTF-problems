8cd95e30e7a13b09cd1bff9dece0f4806186c1c6
keenlooks <keenlooks@gmail.com>
2015-10-25 22:53:21 -0400

    fixed blackhole so that flag is correct and download file is working

--- 2013-Problems/Forensics/Black Hole/challenge.py
+++ 2013-Problems/Forensics/Black Hole/challenge.py
@@ -1,12 +1,7 @@
-from hacksport.problem import Remote, ProtectedFile
+from hacksport.problem import Challenge, File
 import string
 
-class Problem(Remote):
-    program_name = "blackhole.img"
-#    files = [ProtectedFile("flag"), ProtectedFile("key")]
-
-    def initialize(self):
-        # generate random 32 hexadecimal characters
-        self.enc_key = ''.join(self.random.choice(string.digits + 'abcdef') for _ in range(32))
-
-        self.welcome_message = "Welcome to Secure Encryption Service version 1.{}".format(self.random.randint(0,10))
+class Problem(Challenge):
+    files = [File("blackhole.img")]
+    def setup(self):
+        self.flag = "Hacking*Radiation"
